bc1279cd84590a953ab8062239f59276402dfc61
Zhigao Hong <1491537453@qq.com>
2019-04-09 20:12:55 +0800

    多进程（Process类）版

---
+++ apps/process.py
@@ -0,0 +1,48 @@
+import sys
+import multiprocessing
+from socket import *
+
+class Server(object):
+    def __init__(self, port):
+        self.ss = socket(AF_INET, SOCK_STREAM)
+        self.ss.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1) # 开启端口立即复用
+        self.ss.bind(('', port))
+        self.ss.listen(10)
+
+    def __del__(self):
+        print('Sever is closing......')
+        self.ss.close()
+    
+    def run(self):
+        print('Server is starting......')
+        while True:
+            new_client, new_addr = self.ss.accept()
+            print('Welcom, the client', new_addr)
+            # 每来一个客户端就创建一个进程处理之
+            p = multiprocessing.Process(target=self.handle_client, args=(new_client,))
+            p.start()
+            # 因为多进程是资源的深拷贝，所以新的子进程也会有一个client套接字，故该套接字可以关闭
+            new_client.close()
+    
+    def handle_client(self, client):
+        while True:
+            recv_data = client.recv(1024)
+            # 收到的数据为空时表明客户端已断开连接
+            if recv_data:
+                client.send('Already have received:'.encode('utf-8'))
+                client.send(recv_data)
+            else:
+                print('The client has been closed.')
+                client.close()
+                break
+
+def main():
+    if len(sys.argv) == 1:
+        port = 8080
+    else:
+        port = sys.argv[1]
+    server = Server(port)
+    server.run()
+
+if __name__ == '__main__':
+    main()
